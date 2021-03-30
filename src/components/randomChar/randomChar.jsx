6f9bc1b736094b571c67e9614aacc06121fff69a
St.Anton <striuchkov.anton@gmail.com>
2021-03-30 17:18:43 +0300

    Start project

--- src/components/randomChar/randomChar.jsx
+++ src/components/randomChar/randomChar.jsx
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
-import "./charDetails.css";
-export default class CharDetails extends Component {
+import "./randomChar.css";
+
+export default class RandomChar extends Component {
 	render() {
 		return (
-			<div className="char-details rounded">
-				<h4>John Snow</h4>
+			<div className="random-block rounded">
+				<h4>Random Character: John</h4>
 				<ul className="list-group list-group-flush">
 					<li className="list-group-item d-flex justify-content-between">
-						<span className="term">Gender</span>
+						<span className="term">Gender </span>
 						<span>male</span>
 					</li>
 					<li className="list-group-item d-flex justify-content-between">
-						<span className="term">Born</span>
-						<span>1783</span>
+						<span className="term">Born </span>
+						<span>11.03.1039</span>
 					</li>
 					<li className="list-group-item d-flex justify-content-between">
-						<span className="term">Died</span>
-						<span>1820</span>
+						<span className="term">Died </span>
+						<span>13.09.1089</span>
 					</li>
 					<li className="list-group-item d-flex justify-content-between">
-						<span className="term">Culture</span>
-						<span>First</span>
+						<span className="term">Culture </span>
+						<span>Anarchy</span>
 					</li>
 				</ul>
 			</div>
